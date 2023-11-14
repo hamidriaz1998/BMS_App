6a03ced84f5ff36a8c75b886f2c7dfc012d33ebc
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-14 13:51:25 +0500

    Initial Commit

---
+++ App.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void printBanner();
+int startingPage();
+int loginPage();
+main()
+{
+}
+void printBanner()
+{
+    system("cls");
+    cout << "___________________________________________________________________" << endl;
+    cout << "   ,   ,      ____              _        _" << endl;
+    cout << "  /////|     | __ )  ___   ___ | | _____| |__   ___  _ __ " << endl;
+    cout << " ///// |     |  _ \\ / _ \\ / _ \\| |/ / __| '_ \\ / _ \\| '_ \\" << endl;
+    cout << "|~~~|  |     | |_) | (_) | (_) |   <\\__ \\ | | | (_) | |_) |" << endl;
+    cout << "|===|  |     |____/ \\___/ \\___/|_|\\_\\___/_| |_|\\___/| .__/" << endl;
+    cout << "|   |  |                                            |_|" << endl;
+    cout << "|   |  |" << endl;
+    cout << "|   | /" << endl;
+    cout << "|===|/" << endl;
+    cout << "'---'" << endl;
+    cout << "___________________________________________________________________" << endl;
+    cout << endl;
+}
+
+int startingPage()
+{
+    cout << "Welcome Back" << endl;
+    cout << "Choose one of the following......" << endl;
+    cout << "1. Login" << endl;
+    cout << "2. Sign Up" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Your choice (1 or 2): ";
+    int choice;
+    cin >> choice;
+    return choice;
+}

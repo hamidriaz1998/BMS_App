31eb4d7756ea44193e0fe37ae9e00e2eb581015b
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-11-14 14:37:06 +0500

    Complete Sign Up functions

--- App.cpp
+++ App.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
+// Start
 void printBanner();
 int startingPage();
+// Login and Signup
 string inputUsername();
 string inputPassword();
 char inputRole();
+bool login();
+void signUp();
+// Validation
+int searchArray(string arr[], string object);
+// Data Structures
+string usernames[], passwords[];
+char roles[];
+int userCount = 0;
 main()
 {
+    int choice = startingPage();
+    if (choice == 3)
+    {
+        cout << "Exiting..........";
+        return 0;
+    }
+    else if (choice == 1)
+    {
+        printBanner();
+        string username = inputUsername();
+        string password = inputPassword();
+        char role = inputRole();
+    }
 }
 void printBanner()
 {
@@ -39,6 +62,7 @@ int startingPage()
     cin >> choice;
     return choice;
 }
+
 string inputUsername()
 {
     string username;
@@ -54,9 +78,47 @@ string inputPassword()
     return password;
 }
 
-char inputRole(){
+char inputRole()
+{
     char role;
-    cout<<"Role:\ta)Admin\tb)Salesman";
-    cin>>role;
+    cout << "Role:\ta)Admin\tb)Salesman";
+    cin >> role;
     return role;
+}
+bool login()
+{
+    printBanner();
+    string username = inputUsername();
+    string password = inputPassword();
+    int index = searchArray(usernames, username);
+    if (username == usernames[index] && password == passwords[index])
+    {
+        return true;
+    }
+    return false;
+}
+
+void signUp()
+{
+    printBanner();
+    usernames[userCount] = inputUsername();
+    passwords[userCount] = inputPassword();
+    roles[userCount] = inputRole();
+    userCount++;
+    cout << "You have been signed up" << endl;
+    cout << "Press any key to continue.......";
+    getch();
+}
+
+// If object is not in array it will return -1, that can be used to add a condition that it does not exist.
+int searchArray(string arr[], string object)
+{
+    for (int i = 0; i <= userCount; i++)
+    {
+        if (arr[i] == object)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
